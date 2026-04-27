dcad0dcd502708ee25c2242d65324405ce48ee7f
Nooi2572007 <ooi2572007@stu.o-hara.ac.jp>
2026-04-27 14:07:16 +0900

    成績関連のbeenができました。米倉

---
+++ src/main/java/bean/Test.java
@@ -0,0 +1,65 @@
+package bean;
+
+public class Test {
+	private String studentNo;
+	
+	private String schoolCd;
+	
+	private String subjectCd;
+	
+	private int no;
+	
+	private int point;
+	
+	private String classNum;
+
+	public String getStudentNo() {
+		return studentNo;
+	}
+
+	public void setStudentNo(String studentNo) {
+		this.studentNo = studentNo;
+	}
+
+	public String getSchoolCd() {
+		return schoolCd;
+	}
+
+	public void setSchoolCd(String schoolCd) {
+		this.schoolCd = schoolCd;
+	}
+
+	public String getSubjectCd() {
+		return subjectCd;
+	}
+
+	public void setSubjectCd(String subjectCd) {
+		this.subjectCd = subjectCd;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	public String getClassNum() {
+		return classNum;
+	}
+
+	public void setClassNum(String classNum) {
+		this.classNum = classNum;
+	}
+	
+	
+}
